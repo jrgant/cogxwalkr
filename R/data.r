75b0f27e88e70d23fd0042d2d247a594ba64c798
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-04-15 22:50:37 -0400

    add cogsim dataset and documentation

---
+++ R/data.r
@@ -0,0 +1,15 @@
+#' Simulated cognitive data
+#'
+#' A simulated dataset based on a subset of the ADNIMERGE dataset from the
+#' Alzheimer's Disease Neuroimaging Initiative (ADNI)
+#'
+#' @format ## `cogsim`
+#' A data.table containing 1,557 rows and 4 columns:
+#' \describe{
+#'   \item{subid}{Integer. Subject ID}
+#'   \item{dementia}{Integer. Binary indicator of dementia}
+#'   \item{mmse}{Integer. Analagous to MMSE_bl from ADNIMERGE}
+#'   \item{moca}{Integer. Analagous to MOCA_bl from ADNIMERGE}
+#' }
+#' @source <https://adni.loni.usc.edu>
+"cogsim"

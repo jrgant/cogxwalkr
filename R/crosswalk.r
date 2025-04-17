86830fae211a86cf242e864e9f008dbf773d1bc7
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-04-17 16:38:52 -0400

    add analyze_splits()

---
+++ R/crosswalk.r
@@ -0,0 +1,50 @@
+#' Generate data splits
+#'
+#' @param cog1 The name of the first cognitive measure column
+#' @param cog2 The name of the second cognitive measure column
+#' @param data A data.table or data.frame containing the cognitive measure data
+#' @param num_iter Number of split iterations to conduct
+#' @param type One of "unconditional" or "conditional". A conditional split will conduct
+#'   the splits independently within levels of an auxiliary variable (see `condition_by`).
+#' @param condition_by The name of a conditioning variable by which splits will be
+#'   conducted when type="conditional"
+#'
+#' @import data.table
+#' @export
+analyze_splits <- function(cog1, cog2, data, num_iter,
+                           type = "unconditional",
+                           condition_by = NULL) {
+
+  if (type == "unconditional" && !is.null(condition_by)) {
+    warning(
+      "Unconditional split requested, but a conditioning variable is indicated in ",
+      "`condition_by`."
+    )
+  }
+
+  N_INPUT <- nrow(data)
+
+  ## create a dataset of num_iter stacked replicates
+  tmp <- data[sample(seq_len(N_INPUT), size = N_INPUT * num_iter, replace = TRUE)]
+
+  SPLIT_POINT <- floor(N_INPUT / 2)
+  tmp[, `:=`(
+    split = rep(
+      c(rep(1, SPLIT_POINT), rep(2, N_INPUT - SPLIT_POINT)),
+      times = num_iter
+    ),
+    iteration = rep(seq_len(num_iter), each = N_INPUT)
+  )]
+  tmp[]
+
+  ## calculate the mean difference in the cognitive measures by split
+  diffs <- tmp[, .(
+    cog1 = mean(get(cog1)[split == 1]) - mean(get(cog2)[split == 2]),
+    cog2 = mean(get(cog2)[split == 1]) - mean(get(cog2)[split == 2])
+  ), keyby = iteration]
+
+  ## estimate correlation between the cognitive measure differences
+  fit <- lm(cog2 ~ cog1 - 1, data = diffs)
+  list(fit = fit, diffs = diffs)
+}
+

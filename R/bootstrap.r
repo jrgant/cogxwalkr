adab38f2a1bf073f8df375a7e826338a680a08c0
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-05-11 19:54:01 -0400

    remove sample_est option

--- R/bootstrap.r
+++ R/bootstrap.r
@@ -5,7 +5,6 @@
 #' @param num_cores Number of cores to use in parallel processing
 #' @param rng_seed Seed used by doRNG to generate reproducible parallel computations
 #' @param alpha Alpha level for bootstrap confidence intervals
-#' @param sample_est The coefficient from a split procedure conducted in the original
 #'
 #' @import data.table
 #' @import doParallel
@@ -15,9 +14,9 @@
 
 ## FIXME: [2025-04-28] : conditional bootstrapping appears to give more precise
 ##   results than Sarah's paper
-bootstrap_crosswalk <- function(..., num_boot, num_cores = 1, rng_seed,
-                                alpha = 0.05, sample_est = NULL) {
+bootstrap_crosswalk <- function(..., num_boot, num_cores = 1, rng_seed, alpha = 0.05) {
   dcopy <- eval(match.call()$data)
+
   registerDoParallel(num_cores)
   split_data <- foreach(i = seq_len(num_boot),
                         .inorder = FALSE,
@@ -29,26 +28,22 @@ bootstrap_crosswalk <- function(..., num_boot, num_cores = 1, rng_seed,
   }
   stopImplicitCluster()
 
-  percentile_bootstrap_ci(split_data,
-                          alpha = alpha,
-                          sample_est = sample_est)
+  percentile_bootstrap_ci(split_data, alpha = alpha)
 }
 
 
 #' Percentile method
 #' @param bootdist A vector containing the bootstrap distribution
 #' @param alpha Alpha level to use for confidence interval calculation
-#' @param sample_est The coefficient from a split procedure conducted in the original
-#'   sample
 #'
 #' @rdname bootstrap_ci_methods
-percentile_bootstrap_ci <- function(bootdist, alpha = 0.05, sample_est = NULL) {
+percentile_bootstrap_ci <- function(bootdist, alpha = 0.05) {
   ql <- quantile(bootdist, c(alpha / 2, 1 - (alpha / 2)))
-  citab <- data.table(method = "percentile", ci_alpha = alpha,
-                      ll = ql[1], ul = ql[2], se = sd(bootdist))
-  if (!is.null(sample_est)) {
-    citab[, estimate := sample_est]
-    setcolorder(out, c("method", "estimate"))
-  }
+  citab <- data.table(method = "percentile",
+                      ci_alpha = alpha,
+                      ll = ql[1],
+                      ul = ql[2],
+                      se = sd(bootdist))
+
   list(dist = as.vector(bootdist), ci = citab)
 }

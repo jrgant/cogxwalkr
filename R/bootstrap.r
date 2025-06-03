1e653f7d0f2f2f44b9a77a21c64f2630d6dd6dfc
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-06-02 21:38:28 -0400

    rework boots; added summary & print methods

--- R/bootstrap.r
+++ R/bootstrap.r
@@ -35,17 +35,27 @@ bootstrap_crosswalk <- function(..., num_boot, num_cores = 1L, rng_seed) {
 }
 
 
-#' Percentile method
-#' @param bootdist A vector containing the bootstrap distribution
+#' Calculate bootstrapped confidence limits
+#'
+#' @param cx An object of class "cogxwalkr", i.e., an object returned by `crosswalk()`.
 #' @param alpha Alpha level to use for confidence interval calculation
+#' @param type Type of confidence limits to calculate. Currently supported:
+#'   percentile, normal.
 #'
 #' @rdname bootstrap_ci_methods
-percentile_bootstrap_ci <- function(bootdist, alpha = 0.05) {
-  ql <- quantile(bootdist, c(alpha / 2, 1 - (alpha / 2)))
-  citab <- data.table(method = "percentile",
-                      alpha = alpha,
-                      ll = ql[1],
-                      ul = ql[2],
-                      se = sd(bootdist))
-  citab
+bootstrap_ci <- function(cx, alpha = 0.05, type = "percentile") {
+  out <- list()
+  out$alpha <- alpha
+  out$se <- sd(cx$boot$dist)
+  if ("normal" %in% type) {
+    COEF <- unname(coef(cx$fit))
+    ZQT <- qnorm(alpha / 2, lower.tail = FALSE)
+    out$normal <- list(ll = COEF - ZQT * out$se,
+                       ul = COEF + ZQT * out$se)
+  }
+  if ("percentile" %in% type || "perc" %in% type) {
+    ql <- quantile(cx$boot$dist, c(alpha / 2, 1 - (alpha / 2)))
+    out$percentile <- list(ll = unname(ql[1]), ul = unname(ql[2]))
+  }
+  out
 }
--- R/crosswalk.r
+++ R/crosswalk.r
@@ -38,9 +38,10 @@ crosswalk <- function(cog1, cog2, data, num_iter = NULL,
   ## estimate correlation between the cognitive measure differences
   fml <- paste(cog2, "~", cog1, "- 1")
   fit <- lm(as.formula(fml), data = diffs)
+  condition_var <- ifelse(!is.null(condition_by), condition_by, "")
 
   ## store model fit and mean differences
-  out <- list(fit = fit, diffs = diffs)
+  out <- list(fit = fit, diffs = diffs, condition_var = condition_var)
 
   if (!missing("boot_control")) {
     arglist <- as.list(match.call())[-1]
@@ -48,10 +49,8 @@ crosswalk <- function(cog1, cog2, data, num_iter = NULL,
     control[["alpha"]] <- NULL
     # drop boot_control so that we don't re-enter this loop during the bootstraps
     argsboot <- c(arglist[-which(names(arglist) %in% "boot_control")], control)
-    out[["boot"]][["dist"]] <- do.call("bootstrap_crosswalk", args = argsboot)
-    # calculate stats
-    ALPHA <- ifelse(!is.null(boot_control$alpha), boot_control$alpha, 0.05)
-    out[["boot"]][["stats"]] <- percentile_bootstrap_ci(out[["boot"]][["dist"]], ALPHA)
+    out[["boot"]][["dist"]] <- unname(do.call("bootstrap_crosswalk", args = argsboot))
+    attr(out[["boot"]][["dist"]], "coef") <- cog1
   }
 
   class(out) <- c("cogxwalkr", "list")
@@ -61,15 +60,50 @@ crosswalk <- function(cog1, cog2, data, num_iter = NULL,
 
 #' Summarize a cogxwalkr list
 #'
-#' @import data.table
+#' @param cx An object of class "cogxwalkr", i.e., an object returned by `crosswalk()`.
+#' @param alpha Alpha to use for confidence interval calculation. Defaults to 0.05.
+#' @param bci_type Type of bootstrapped confidence interval to calculate. Currently
+#'   accepts "percentile" (or "perc") and/or "normal." Both are included by default.
+#'
 #' @export
-summary.cogxwalkr <- function(object) {
-  est <- coef(object$fit)
-  out <- data.table(sample_est = unname(est))
-  if (!is.null(object$boot)) {
-    out <- cbind(out, object$boot$stats)
-    out[, boot_est := mean(object$boot$dist)]
-    setcolorder(out, c("sample_est", "boot_est"))
+summary.cogxwalkr <- function(cx, alpha = 0.05, bci_type = c("percentile", "normal")) {
+  out <- list(
+    fml = paste(c(terms(cx$fit)[[2]], "~", terms(cx$fit)[[3]]), collapse = " "),
+    sample_est = unname(coef(cx$fit)),
+    condition_var = ifelse(cx$condition_var == "", "none", cx$condition_var),
+    num_iter = nrow(cx$diffs)
+  )
+  if (!is.null(cx$boot)) {
+    out$num_boot <- length(cx$boot$dist)
+    out$boot_est  <-  mean(cx$boot$dist)
+    out$ci <- bootstrap_ci(cx, alpha, type = bci_type)
   }
-  out[]
+  class(out) <- c("summary.cogxwalkr", "list")
+  out
+}
+
+
+#' Print a cogxwalkr summary
+#'
+#' @param digits Number of digits to show. Passed to `round()`.
+#'
+#' @export
+print.summary.cogxwalkr <- function(x, digits = 3L) {
+  fd <- function(num) round(num, digits = digits)
+  indent <- paste(rep(" ", 2), collapse = "")
+
+  cat("\n")
+  cat("[Crosswalk formula]", "\n", indent, x$fml, "\n\n")
+  cat("[Coefficient]", "\n", indent, fd(x$sample_est), "\n\n")
+  cat(paste0("[", (1 - x$ci$alpha) * 100, "% confidence limits]"), "\n")
+
+  ci_types <- names(x$ci)[!names(x$ci) %in% c("alpha", "se")]
+  sapply(ci_types, \(.x) {
+    tmp <- x[["ci"]][[.x]]
+    cat(indent, paste0("(", fd(tmp$ll), ", ", fd(tmp$ul), ")"), "-", .x, "\n")
+  })
+  cat("\n--------------------------------------------------\n")
+  cat("Number of boostrap replicates:", x$num_boot, "\n")
+  cat("Number of iterations:", x$num_iter, "\n")
+  cat("Conditioning variable:", x$condition_var, "\n\n")
 }

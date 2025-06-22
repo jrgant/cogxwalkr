f07c4dc5c5c545059c321dcd8f8d8e2110c42aa9
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-06-22 17:22:27 -0400

    add plot tests

--- tests/testthat/test-plots.r
+++ tests/testthat/test-plots.r
@@ -1,4 +1,12 @@
-test_that("plot method errors out if invalid type is provided", {
+test_that("plot method errors out with bad requests based on crosswalk() output", {
   cx <- crosswalk("mmse", "moca", cogsim)
   expect_error(plot(cx, types = "invalid"))
+  expect_error(plot(cx, types = "boot")) # no bootstrap results present in cx
+
+  cxi <- crosswalk("mmse", "moca", cogsim, niter = 1000)
+  expect_error(plot(cxi, types = "boot")) # diffs, but no bootstrap results present in cxi
+
+  cxb <- crosswalk("mmse", "moca", cogsim, niter = 1000,
+                   control = list(nboot = 10, seed = 123))
+  expect_error(plot(cxb, cxsum = summary(cxb), citype = c("percentile", "normal")))
 })

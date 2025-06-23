c35217e5711a788e9612c9328df7d56f66542812
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-06-23 09:24:32 -0400

    add tests

--- tests/testthat/test-plots.r
+++ tests/testthat/test-plots.r
@@ -10,3 +10,12 @@ test_that("plot method errors out with bad requests based on crosswalk() output"
                    control = list(nboot = 10, seed = 123))
   expect_error(plot(cxb, cxsum = summary(cxb), citype = c("percentile", "normal")))
 })
+
+test_that("plot method behaves as intended for valid configurations", {
+  cxb <- crosswalk("mmse", "moca", cogsim, control = list(nboot = 10, seed = 123))
+  expect_message(plot(cxb, cxsum = summary(cxb))) # no split data should induce message
+
+  cxb <- crosswalk("mmse", "moca", cogsim, niter = 1000,
+                   control = list(nboot = 10, seed = 123))
+  expect_no_failure(plot(cxb, cxsum = summary(cxb)))
+})

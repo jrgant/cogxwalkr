52778b7d6e95847abe6ce169666aad092396097f
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-08-04 10:38:03 -0400

    add parallel reproducibility test

--- tests/testthat/test-bootstrap.r
+++ tests/testthat/test-bootstrap.r
@@ -31,3 +31,17 @@ test_that("test that bootstrap_crosswalk() respects ncores request", {
   ncores2 <- as.numeric(stringr::str_extract(coreinfo2, "[0-9]+"))
   expect_true(ncores2 >= 1 & ncores2 <= parallel::detectCores())
 })
+
+test_that("test that bootstrap_crosswalk() is reproducible across different `ncores`", {
+  bootbase <- function(...) {
+    bootstrap_crosswalk(
+      cog1 = "mmse", cog2 = "moca", data = cogsim,
+      nboot = 10, seed = 1066, ...
+    )
+  }
+  b1 <- bootbase(ncores = 1)
+  b2 <- bootbase(ncores = 2)
+  b3 <- bootbase(ncores = 999)
+  expect_identical(b1, b2)
+  expect_identical(b2, b3)
+})
